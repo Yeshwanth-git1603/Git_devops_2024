1e946b3d615a4332c69094775df3fc2a0cb5241f
EC2 Default User <ec2-user@ip-172-31-47-173.eu-north-1.compute.internal>
2024-07-27 14:13:18 +0000

    classes

---
+++ classes/class.py
@@ -0,0 +1,9 @@
+class basic:
+    def programming(self,name,created):
+        self.name=name
+        self.created=created
+        print(self.name,self.created)
+
+
+b=basic()
+b.programming("python","guido van russum")
